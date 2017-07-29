97be6ec09433fa31f88cc784bc748a40bf6d3180
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-07-29 16:16:57 +1000

    Added basic socket.io connect logic

--- src/client/setupSocket.jsx
+++ src/client/setupSocket.jsx
@@ -4,9 +4,14 @@ import { keys } from './socket-keys.jsx';
  */
 function setupSocket(socket, mapState) {
   socket.on(keys.retrievedCrashData, (crashDataList) => {
+    console.log("Retrieved data: " );
+    console.log(crashDataList);
     // Since we're using MobX this will automatically trigger a rerender :D
     mapState.crashDataList = crashDataList;
   });
+  socket.on('connection', () => {
+    console.log("Connected to socketio");
+  })
 }
 export { setupSocket }
 export default setupSocket;
