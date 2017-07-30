05decd07007d6347364caa37bb679703c1c84a72
Eric Jiang <ericjiang97@gmail.com>
2017-07-30 13:18:32 +1000

    added Traffic Layer

--- src/view/HeatMap.jsx
+++ src/view/HeatMap.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { tileLayer, map, LatLng } from 'leaflet';
 import { mapState } from '../state/appState';
 import { observer } from 'mobx-react';
+import "mapbox.js"
 import './HeatMap.css';
 import './leaflet-heat.js';
 const initialLocation = { lat: -37.81425, lng: 144.9632 };
 
+const MapBoxOptions = {
+    accessToken: "pk.eyJ1IjoibG9yZGVyaWtpciIsImEiOiJjaXFkOGk0dGswMmRsZmpucDdrdXkweTZ2In0.iPdqtjwZaw2gHQG3bxwSmQ"
+}
 @observer
 class HeatMap extends React.Component {
   constructor() {
@@ -21,11 +25,13 @@ class HeatMap extends React.Component {
         gradient: {0.2: "#55BBFA", 0.4: "#FABB55", 0.6: "#FA9955",0.75: "#FF3333", 1: "#FF0000"}
       }
     );
+    this.trafficLayer = L.mapbox.styleLayer('mapbox://styles/lorderikir/cj5q2c46z1g8j2rqnt2qh9uje', MapBoxOptions)
   }
   componentDidMount() {
     this.map = map(this.mapContainer).setView([initialLocation.lat, initialLocation.lng], 13);
     this.tileLayer.addTo(this.map);
     this.heatLayer.addTo(this.map);
+    this.trafficLayer.addTo(this.map);
   }
   render() {
     this.heatLayer.setLatLngs(
